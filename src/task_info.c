2f88aa5b404741020f292820eb7468e05460db99
osboxes.org <dbair@uoregon.edu>
2026-06-03 01:31:28 -0400

    added exe_path functionality to task collection and logs, goal is to add risk engine logic for suspicious processes and suspicious tasks. Hopefully this allows for earlier detection of potential kernal modifications.

--- src/task_info.c
+++ src/task_info.c
@@ -3,6 +3,58 @@
 #include "task_info.h"
 #include"risk_engine.h"
 
+//dependencies for exec_path:
+
+#include <linux/fs.h>
+#include <linux/path.h>
+#include <linux/dcache.h>
+#include <linux/slab.h>
+#include <linux/file.h>
+#include <linux/mm.h>
+#include <linux/rcupdate.h>
+
+void p_fill_task_exec_path(struct task_struct *task,
+                           char *buf,
+                           size_t buf_len)
+{
+    struct file *exe_file;
+    char *tmp_buf;
+    char *path;
+
+    if (!task || !task->mm || !buf || !buf_len)
+        return;
+
+    buf[0] = '\0';
+
+    rcu_read_lock();
+
+    exe_file = rcu_dereference(task->mm->exe_file);
+
+    if (exe_file)
+        get_file(exe_file);
+
+    rcu_read_unlock();
+
+    if (!exe_file)
+        return;
+
+    tmp_buf = kmalloc(PAGE_SIZE, GFP_KERNEL);
+    if (!tmp_buf) {
+        fput(exe_file);
+        return;
+    }
+
+    path = d_path(&exe_file->f_path, tmp_buf, PAGE_SIZE);
+
+    if (!IS_ERR(path))
+        strscpy(buf, path, buf_len);
+    else
+        strscpy(buf, "[path_error]", buf_len);
+
+    kfree(tmp_buf);
+    fput(exe_file);
+}
+
 void p_collect_task_info(struct task_struct *task, p_task_info *info)
 {
     strscpy(info->comm, task->comm, sizeof(info->comm));
@@ -13,6 +65,7 @@ void p_collect_task_info(struct task_struct *task, p_task_info *info)
     info->euid = task->cred->euid;
     info->state = task->__state;
     info->flags = task->flags;
+    p_fill_task_exec_path(task, info->exe_path, sizeof(info->exe_path));
     info->risk_score = process_risk_score(info);
 }
 
@@ -32,4 +85,5 @@ void p_log_task_info(p_task_info *info)
        __kuid_val(info->euid),
        info->state,
        info->flags);
+       printk(KERN_INFO "exe_path=%s\n", info->exe_path);
 }
--- src/task_info.h
+++ src/task_info.h
@@ -4,8 +4,8 @@
 #include <linux/types.h>
 #include <linux/cred.h>
 #include<linux/sched.h>
-
-
+#include <linux/limits.h>
+#include <linux/kernel.h>
 
 typedef struct p_task_info {
     char comm[16];
@@ -16,7 +16,7 @@ typedef struct p_task_info {
     kuid_t euid;
     long state; 
     unsigned int flags;
-    char exe_path[256];
+    char exe_path[PATH_MAX];
     int risk_score; //is a process indicative of an existing rootkit?
 } p_task_info;
 
@@ -24,4 +24,6 @@ typedef struct p_task_info {
 void p_collect_task_info(struct task_struct *task, p_task_info *info);
 
 void p_log_task_info(p_task_info *info);
+
+void p_fill_task_exec_path(struct task_struct *task, char *buf, size_t buf_len);
 #endif
